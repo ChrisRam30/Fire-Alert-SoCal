5cc0b55b91c8e9f3ebd36cedb6329e63ee54c569
chris.ram30 <chris.ram30@gmail.com>
2024-01-22 19:41:03 -0800

    Initial Commit

---
+++ Fire Alert SoCal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fire Alert SoCal
+//
+//  Created by Christopher Ram on 1/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
